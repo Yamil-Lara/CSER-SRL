af958d30115108f469846e0a8e9df039e0bc33e3
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-04-04 17:04:13 -0400

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Profile from './pages/Profile';
+
+// Definimos la "Interface" para que TS sepa qué trae el JSON de Laravel
+interface StatusResponse {
+  status: string;
+  database: string;
+  mensaje: string;
+}
+
+const App: React.FC = () => {
+  const [data, setData] = useState<StatusResponse | null>(null);
+
+  useEffect(() => {
+    axios.get<StatusResponse>('http://127.0.0.1:8000/api/status')
+      .then(response => setData(response.data))
+      .catch(error => console.error("Error de conexión:", error));
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
