d8b09624a2fedf708cb8190da93b3154343a7892
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2026-04-04 17:07:42 -0400

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // Mantenemos los estilos nuevos de la barra lateral
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
